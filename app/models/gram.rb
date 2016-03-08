17a2d2f4be594bf96a858118f80148e2a8af8fc2
rosebuddy <helloladybread@gmail.com>
2016-03-08 09:34:58 +0000

    Create gram comments

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,8 +1,10 @@
 class Gram < ActiveRecord::Base
 
   mount_uploader :picture, PictureUploader
+  
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
 
   belongs_to :user
   has_many :pictures
