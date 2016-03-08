09941034e481301296d08ac67b6e629c66ebfad9
rosebuddy <helloladybread@gmail.com>
2016-03-08 09:19:03 +0000

    Validate picture field on gram

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,7 @@ class Gram < ActiveRecord::Base
 
   mount_uploader :picture, PictureUploader
   validates :message, presence: true
+  validates :picture, presence: true
 
   belongs_to :user
   has_many :pictures
